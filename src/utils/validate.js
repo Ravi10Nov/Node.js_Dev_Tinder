1bfffd46e671d4a648216c8931074ff4b2595e20
Ravi Kant <ravikantas.kant81@gmail.com>
2024-10-23 23:50:22 +0530

    Projects Done

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,11 +1,11 @@
 const validator = require('validator');
 
 const isValidateSignupData = (req) =>{
-    const {firstName , lastName,emailId, password} = req.body;
+    const {firstName , lastName,emailId, password } = req.body;
 
     if (! firstName || !lastName){
         throw new Error('Name is not valid');
-    }
+    } 
     else if( ! validator.isEmail(emailId)){
         throw new Error('Email is not valid!!!');
     }
