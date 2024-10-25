512be12f65cfe29c32d805e4b306170097e16119
Ravi Kant <ravikantas.kant81@gmail.com>
2024-10-25 21:23:46 +0530

    Change origin

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ console.log('Using port:', port);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://dev-tinder-app.vercel.app",
-    // origin:"http://localhost:5173",
+    // origin:"https://dev-tinder-app.vercel.app",
+    origin:"http://localhost:5173",
     credentials:true,
 }))
 
